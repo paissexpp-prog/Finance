2a0760b3f5d9e2742a8de3b04ffcb819221d29a2
root <root@akunv538717949vds.hosted.phylex.net>
2026-01-04 13:53:57 +0000

    hapus plugin konflik vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
 import path from "path";
 import { defineConfig } from "vite";
 
-const plugins = [react(), tailwindcss(), jsxLocPlugin()];
+const plugins = [react(), tailwindcss()];
 
 export default defineConfig({
   plugins,
